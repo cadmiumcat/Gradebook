21d02ab44a8cb8f9b7852799f7dc58ec1bfad7f5
Catalina <catalina.garcia.mail@gmail.com>
2019-07-09 18:12:20 +0100

    Checks references to objects

--- test/Gradebook.Tests/TypeTests.cs
+++ test/Gradebook.Tests/TypeTests.cs
@@ -17,6 +17,23 @@ namespace GradeBook.Tests
             //assert
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject() 
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+
+            //act 
+            var book2 = book1;
+
+            //assert
+            Assert.Same(book1, book2);
+            //the following line tests the same (it's what goes on behind the scenes)
+            Assert.True(Object.ReferenceEquals(book1, book2));
 
         }
 
